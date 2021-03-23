7ed957f24b79b6acdfcb2aa43dabc33f0f4325a8
David <d.pitarch@outlook.es>
2021-03-24 00:39:42 +0100

    Modify geekshubs function adding conditions to 15, 5 and 3 multiples to print diferent words when the arguments in function are equal to multiples

--- test/test01.js
+++ test/test01.js
@@ -1,4 +1,25 @@
 function geekshubs(index) {
-    return "";
+    
+    let item = "";
+
+    for (let i = 1;  i <= index; i ++) {
+        
+        if ( i % 15 == 0) {
+            item += "GeeksHubs\n";
+        } 
+        else if ( i % 5 == 0) {
+            item += "Hubs\n";
+        } 
+        else if ( i % 3 == 0) {
+            item += "Geeks\n";
+        } 
+        else {
+            item += i + "\n";
+        }
+        
+    }
+
+    return item;
 }
+
 module.exports = geekshubs;
